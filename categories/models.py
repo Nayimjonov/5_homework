55e4cacb4b1cd6cc11622972362f468e99c32c74
Samandar <samandarnayimjonov@gmail.com>
2025-04-05 10:00:14 +0500

    category model done

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from categories.models import Category
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='news')
+
