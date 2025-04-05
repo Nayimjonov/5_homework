220948ce14105f2c526931b63af6d4c523f097f6
Samandar <samandarnayimjonov@gmail.com>
2025-04-05 10:21:04 +0500

    comment model done

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from news.models import News
 
-# Create your models here.
+
+class Comment(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=170)
+    author_email = models.EmailField()
+    content = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
