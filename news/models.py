9f748081e0d388caaac2b60827a1e05d4f9acd21
Samandar <samandarnayimjonov@gmail.com>
2025-04-05 10:16:40 +0500

    model update

--- news/models.py
+++ news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from tags.models import Tag
 from categories.models import Category
 
 
@@ -8,4 +8,13 @@ class News(models.Model):
     slug = models.SlugField(unique=True)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='news')
+    tags = models.ManyToManyField(Tag, related_name='news')
+    image = models.ImageField(upload_to='image/', null=True, blank=True)
+    view_count = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
