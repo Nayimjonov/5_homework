37baf213d0fccf97c44047f219636dbe55b6d9fe
Samandar <samandarnayimjonov@gmail.com>
2025-04-06 13:29:43 +0500

    comment admin panel yozildi

--- comments/admin.py
+++ comments/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Comment
 
 
-@admin.register(Comment):
-    pass
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author_name', 'author_email', 'is_approved')
+    search_fields = ('is_approved', 'author_email')
+    list_filter = ('news', 'is_approved')
+
