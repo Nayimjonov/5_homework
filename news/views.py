d2fc253c5c2088f036f4c67f87ca866c779a922a
Samandar <samandarnayimjonov@gmail.com>
2025-04-07 08:48:49 +0500

    news paginator qushildi

--- news/views.py
+++ news/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .models import News
 from .serializers import NewsSerializer
+from .paginations import NewsPagination
 
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    pagination_class = NewsPagination
 
