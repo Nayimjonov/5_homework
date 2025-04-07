ecee9fae2ffaa844c4e424cddecb8b00c388d2d2
Samandar <samandarnayimjonov@gmail.com>
2025-04-07 08:38:31 +0500

    done

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from tags.models import Tag
 from categories.models import Category
 
@@ -14,6 +15,11 @@ class News(models.Model):
     is_published = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(News, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
