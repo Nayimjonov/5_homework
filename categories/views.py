60b65fa8bec386ebe464cd816669279fec0063af
Samandar <samandarnayimjonov@gmail.com>
2025-04-07 06:12:20 +0500

    category views yozibolindi

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ViewSet):
+    
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, pk=None):
+        category = get_object_or_404(Category, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        category = get_object_or_404(Category, pk=pk)
+        serializer = CategorySerializer(Category, category.request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def destroy(self, request, pk=None):
+        category = get_object_or_404(Category, pk=pk)
+        category.delete()
+        return Response(status=204)
+
+
+
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('categories.urls')),
+
 ]
 
 if settings.DEBUG:
